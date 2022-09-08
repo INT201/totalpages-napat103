ce10b57f2fdb5509ee976300a704f6788cda7a53
napat103 <naphat.watt@kmutt.ac.th>
2022-09-08 18:39:23 +0700

    finish

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,18 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  if (arrayItems === undefined || arrayItems === null) {
+    return undefined
+  } else if (rowsPerPage === undefined || rowsPerPage === null || rowsPerPage == 0) {
+    return 1
+  } else {
+    return Math.ceil(arrayItems.length / rowsPerPage)
+  }
 }
+
 module.exports = totalPages
+
+// let b = [item1, item2, item3, ...a, item35]
+// console.log(totalPages(b, 15))
+
+// let a = [1, 2, 3, 10]
